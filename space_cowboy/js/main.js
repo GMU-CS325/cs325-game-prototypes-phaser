c398a5c2818f59115b0aaeceb7886fc16d5b5594
Joshua Wagner <tlrwgnr@gmail.com>
2017-02-07 20:01:03 -0500

    added random powerups chosen from three presets, only bolt actually works, both pill an start need implementation

--- space_cowboy/js/main.js
+++ space_cowboy/js/main.js
@@ -29,8 +29,61 @@ window.onload = function() {
         return asteroid;
     }
 
-    function decrementScore(player, asteroid) {
+    function createPowerup(){
+        var r = Math.random();
+        if(r <= .4){
+            powerupType = 'powerupStar';
+        }else if(r <= .8){
+            powerupType = 'powerupBolt';
+        }else{
+            powerupType = 'powerupPill';
+        }
+        var powerup = powerups.create(game.world.width, ((game.world.height - 128) - (game.world.height * 1 / 2 * Math.random())), powerupType);
+        // powerup.scale.setTo(2,2);
+        powerup.body.velocity.x = cowSpeed/2;
+        return powerup;
+    }
+
+    function collectPowerup(player, currentPowerup){
+        powerupfx.play();
+        switch(powerupType){
+            case 'powerupStar':
+                player.body.gravity.y = 0;
+                game.time.events.add(Phaser.Timer.SECOND * 6, resetPowerup, this);
+                break;
+            case 'powerupPill':
+                break;
+            case 'powerupBolt':
+                weapon.fireRate = 300;
+                game.time.events.add(Phaser.Timer.SECOND * 6, resetPowerup, this);
+                break;
+            default:
+                break;
+        }
+        // var tween = game.add.tween(weapon).to({fireRate:1000},1000,true);
+        currentPowerup.kill();
+    }
+
+    function resetPowerup(){
+        switch(powerupType){
+            case 'powerupStar':
+                player.body.gravity.y = 800;
+                break;
+            case 'powerupPill':
+                break;
+            case 'powerupBolt':
+                weapon.fireRate = 1000;
+                break;
+            default:
+                break;
+        }
+        powerupType = 'none';
+
+    }
+
+    function playerHit(player, asteroid) {
         asteroid.kill();
+
         for(let i = 0; i < 10; i++){
             player.alpha = 0;
             var tween = game.add.tween(player).to( { alpha: 1 }, 2000, "Linear", true);
@@ -59,19 +112,30 @@ window.onload = function() {
     var laser;
     var moo;
     var jumpfx;
+    // var hitfx;
     var powerupfx;
+    var powerups;
+    var powerupType;
 
 
     function preload() {
+        // game image assets
         game.load.image('sky', 'assets/earth_sky.png');
         game.load.image('ground', 'assets/moon_surface.png');
         game.load.image('asteroid', 'assets/spr_cow_0.png');
         game.load.spritesheet('astronaut', 'assets/orange.png', 128,128);
         game.load.image('laserblue', 'assets/laserBlue01.png');
+        game.load.image('powerupBolt', 'assets/bold_silver.png');
+        game.load.image('powerupPill', 'assets/pill_blue.png');
+        game.load.image('powerupStar', 'assets/star_gold.png');
+
+        // game sounds
         game.load.audio('sfx', 'assets/laser.wav');
         game.load.audio('moofx', 'assets/cow_moo.wav');
         game.load.audio('jumpfx', 'assets/jump.wav');
         game.load.audio('powerupfx', 'assets/powerup.wav');
+
+
         points = 0;
     }
 
@@ -83,22 +147,22 @@ window.onload = function() {
         sky = game.add.sprite(0, 0, 'sky');
         sky.scale.setTo(1,1);
 
+        // add audio triggers
         laser = game.add.audio('sfx');
         moo = game.add.audio('moofx');
         jumpfx = game.add.audio('jumpfx');
         powerupfx = game.add.audio('powerupfx');
 
-        // game.add.sprite(0,0, 'asteroid')
+        // create groups
+        powerups = game.add.group();
+        powerups.enableBody = true;
 
         asteroids = game.add.group();
         asteroids.enableBody = true;
 
-        // var asteroid = asteroids.create(game.world.width, game.world.height*.75, 'asteroid');
-        // asteroid.body.gravity.y = 20;
-        // asteroid.body.velocity.x = -50;
-        // asteroid.body.bounce.y = 0;
-
         game.time.events.repeat(Phaser.Timer.SECOND * cowRate, 1000, createAsteroid, this);
+        game.time.events.repeat(Phaser.Timer.SECOND * cowRate*8, 1000, createPowerup, this);
+        // game.time.events.repeat(Phaser.Timer.SECOND * 10, 1000, resetPowerup, this);
 
         scoreText = game.add.text(16, 16, 'score: 0', {
             fontSize: '32px',
@@ -110,10 +174,7 @@ window.onload = function() {
         ground = platforms.create(0, game.world.height - 64, 'ground');
         ground.scale.setTo(.5, 1);
         ground.body.immovable = true;
-        // var ledge = platforms.create(400, 400, 'ground');
-        // ledge.body.immovable = true;
-        // ledge = platforms.create(-150, 200, 'ground');
-        // ledge.body.immovable = true;
+
 
         player = game.add.sprite(32, game.world.height/2, 'astronaut');
         game.physics.arcade.enable(player);
@@ -144,12 +205,11 @@ window.onload = function() {
             cowSpeed -= 50;
             lastPoints = points;
         }
-        // game.physics.arcade.collide(asteroids, ground);
-        // getting asteroid collision with world bounds and decrementing player score
-        // asteroids.forEach(decrementScore);
-        game.physics.arcade.collide(player, asteroids, decrementScore);
+
+        game.physics.arcade.collide(player, asteroids, playerHit);
         game.physics.arcade.collide(weapon.bullets, asteroids, asteroidHit, null, this);
-        // game.physics.arcade.collide(weapon, asteroids, function(bullet, asteroid){bullet.kill(); asteroid.kill();});
+        game.physics.arcade.collide(player, powerups, collectPowerup);
+
 
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         cursors = game.input.keyboard.createCursorKeys();
@@ -167,13 +227,14 @@ window.onload = function() {
         }
 
         if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-            player.body.velocity.y = -500;
+            player.body.velocity.y = -700;
             jumpfx.play();
         }
 
         if (fireButton.isDown) {
             if(weapon.fire()){
                 laser.play();
+                // createPowerup();
             }
 
         }
