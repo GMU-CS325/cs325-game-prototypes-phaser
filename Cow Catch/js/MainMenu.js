98d744b2470cfca4b99e3c373eacad94d2a30c1f
Shierell Yalong <syalong@masonlive.gmu.edu>
2017-02-07 19:36:10 -0500

    AGH background isn't listening

--- Cow Catch/js/MainMenu.js
+++ Cow Catch/js/MainMenu.js
@@ -17,7 +17,7 @@ BasicGame.MainMenu.prototype = {
 		this.music = this.add.audio('titleMusic');
 		this.music.play();
 
-		this.add.sprite(0, 0, 'titlePage');
+		this.add.sprite(0, 0, 'background');
 
 		this.playButton = this.add.button( 303, 400, 'playButton', this.startGame, this, 'over', 'out', 'down');
 
