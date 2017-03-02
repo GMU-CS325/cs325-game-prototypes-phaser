0a54f335356220a5010ba846a3290c3bb0964e4d
FlavioAmurrioCS <FlavioAmurrioCS@gmail.com>
2017-03-02 11:22:34 -0500

    hmm

--- sentPhoneSnobPreserv/js/main.js
+++ sentPhoneSnobPreserv/js/main.js
@@ -1,6 +1,6 @@
 var SideScroller = SideScroller || {};
 
-SideScroller.game = new Phaser.Game(746, 420, Phaser.AUTO, '');
+SideScroller.game = new Phaser.Game(746, 420, Phaser.AUTO, 'game');
 
 SideScroller.game.state.add('Boot', SideScroller.Boot);
 SideScroller.game.state.add('Preload', SideScroller.Preload);
