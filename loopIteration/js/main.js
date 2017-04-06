cc317e252803a25ef59d812f1874c91f4101a9ea
FlavioAmurrioCS <FlavioAmurrioCS@gmail.com>
2017-04-06 10:19:02 -0400

    IterationLoop

---
+++ loopIteration/js/main.js
@@ -0,0 +1,130 @@
+"use strict";
+
+window.onload = function() {var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.image('logo', 'assets/phaser.png');
+        game.load.spritesheet('fatEagle', 'assets/fatEagle125x72.png',125, 72);
+        game.load.image('ground', 'assets/ground.png');
+        game.load.image('poopIMG', 'assets/poopIMG.png');
+    }
+    
+    var player;
+    var topText;
+    var poops;
+    var platforms;
+    var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+    var cursors;
+    var size = 0.5;
+    var rightMov = true;
+    
+    function create() {
+        game.stage.backgroundColor = "#4488AA";
+        
+        
+        
+        platforms = game.add.group();
+        platforms.enableBody = true;
+        var ground = platforms.create(0, game.world.height - 30, 'ground');
+//        ground.scale.setTo(2, 2);
+        ground.body.immovable = true;
+        
+        player = game.add.sprite(game.world.centerX, game.world.centerY, 'fatEagle');
+        player.anchor.setTo(0.5, 0);
+        player.scale.setTo(size);
+        game.physics.arcade.enable(player);
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 20;
+        player.body.collideWorldBounds = true;
+        player.animations.add('left', [0,1,2,3], 10, true);
+        player.animations.add('right', [0,1, 2,3], 10, true);
+        //player.rotate(.3);
+        
+        // Turn on the arcade physics engine for this sprite.
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        player.body.collideWorldBounds = true;
+        
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        
+        var text = game.add.text(game.world.centerX, 15, "Attack Eagle", style);
+        text.anchor.setTo(0.5, 0.0);
+        
+        poops = game.add.group();
+        poops.enableBody = true;
+//        for (var i = 0; i < 4; i++)
+//        {
+//            var pop = poops.create(i * 180, 0, 'poopIMG');
+//            pop.scale.setTo(.08);
+//
+//            pop.body.gravity.y = 300;
+//
+////            pop.body.bounce.y = 0.7 + Math.random() * 0.2;
+//        }
+         cursors = game.input.keyboard.createCursorKeys();
+    }
+    
+    
+    function update() {
+        game.physics.arcade.collide(player, platforms);
+        game.physics.arcade.collide(poops, platforms);
+
+        //game.physics.arcade.overlap(player, poops, growUp, null, this);
+
+        player.body.velocity.x = 0;
+        if (game.input.keyboard.isDown(Phaser.Keyboard.S)){
+            player.scale.setTo(2*size, 2*size); 
+        }
+
+        if (cursors.left.isDown)
+        {
+            rightMov = false;
+            player.body.velocity.x = -250;
+            player.scale.setTo(-size, size);
+
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            rightMov = true;
+            player.body.velocity.x = 250;
+            player.scale.setTo(size, size);
+            
+
+            player.animations.play('right');
+            
+        }
+        else
+        {
+            //player.animations.stop();
+
+            //player.frame = 9;
+        }
+
+        if (cursors.up.isDown) //&& player.body.touching.down)
+        {
+            player.body.velocity.y = -50;
+            //player.frame = 19;
+            if(rightMov)
+            {
+                player.animations.play('right');       
+            }
+            else{
+                player.animations.play('left');
+            }
+        }
+        if (cursors.down.isDown) //&& player.body.touching.down)
+        {
+            player.body.velocity.y = 50;
+            //player.frame = 19;
+            if(rightMov)
+            {
+                player.animations.play('right');       
+            }
+            else{
+                player.animations.play('left');
+            }
+        }
+    }
+};
