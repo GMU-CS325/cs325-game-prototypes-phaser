fa02d7fd97b1c61746d50f4826d6d9300c7bc723
FlavioAmurrioCS <FlavioAmurrioCS@gmail.com>
2017-06-30 16:15:46 -0400

    Added New Place
    for ttimer

---
+++ TTimer/js/main.js
@@ -0,0 +1,44 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update});
+
+function preload() {
+    game.load.audio('boden', ['assets/F-Zero - Death Wind.mp3']);
+    
+}
+
+
+var music;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#313131';
+
+    music = game.add.audio('boden');
+
+    music.play();
+
+    game.input.onDown.add(changeVolume, this);
+
+}
+
+function changeVolume(pointer) {
+
+    if (pointer.y < 100)
+    {
+        music.mute = false;
+    }
+    else if (pointer.y < 300)
+    {
+        music.volume += 0.1;
+    }
+    else
+    {
+        music.volume -= 0.1;
+    }
+
+}
+
+function update() {
+
+}
