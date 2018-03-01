6c8fd0ffc3e1e2decf9716101cdcf75997006701
jlee184 <jlee184@gmu.edu>
2018-03-01 14:52:52 -0500

    Assignment 4

--- Digital Assignment 4/js/main.js
+++ Digital Assignment 4/js/main.js
@@ -1,6 +1,7 @@
-//"use strict";
+"use strict";
 
-window.onload = function() {
+window.onload = function()
+{
     // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
     // You will need to change the fourth parameter to "new Phaser.Game()" from
     // 'phaser-example' to 'game', which is the id of the HTML element where we
@@ -13,221 +14,369 @@ window.onload = function() {
     // mods by Patrick OReilly 
     // twitter: @pato_reilly
 
-	var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update });
+	var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
-	function preload() {
-
-	game.load.image('atari', 'assets/block.png');
-	game.load.image('background', 'assets/background2.png');
-	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-
-	}
-
-	var sprite1;
-	var sprite2;
-	var sprite3;
-	var sprite4;
-
-	function create() {
-
-	    game.stage.backgroundColor = '#2d2d2d';
-
-	    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-	    //  Set the world (global) gravity
-	    game.physics.arcade.gravity.y = 100;
-
-	    //  Sprite 1 will use the World (global) gravity
-	    sprite1 = game.add.sprite(100, 96, 'ilkke');
-
-	    //  Sprite 2 is set to ignore the global gravity and use its own value
-	    sprite2 = game.add.sprite(300, 96, 'ilkke');
-
-	    //  Sprite 3 will use both the world gravity and its own gravityScale modifier
-	    sprite3 = game.add.sprite(500, 96, 'ilkke');
-
-	    //  Sprite 4 will ignore all gravity
-	    sprite4 = game.add.sprite(700, 96, 'ilkke');
-
-	    // Enable physics on those sprites
-	    game.physics.enable( [ sprite1, sprite2, sprite3, sprite4 ], Phaser.Physics.ARCADE);
-
-	    sprite1.body.collideWorldBounds = true;
-	    sprite1.body.bounce.y = 0.8;
+	function preload()
+	{
+		game.load.spritesheet('button', 'assets/Play.png', 200, 90);
+		game.load.spritesheet('arrow', 'assets/Arrow.png', 101, 50);
+	    game.load.image('background','assets/Background.png');
+	    game.load.spritesheet('husky','assets/Husky.png', 90, 58, 3);
 	    
-	    sprite2.body.collideWorldBounds = true;
-	    sprite2.body.bounce.y = 0.8;
-	    sprite2.body.gravity.y = 200;
-	    
-	    sprite3.body.collideWorldBounds = true;
-	    sprite3.body.bounce.y = 0.8;
-	    sprite3.body.gravity.y = 50;
-
-	    sprite4.body.allowGravity = false;
-
 	}
 
-	function render() {
+	var button1;
+	var button2;
+	var button3;
+	var button4;
+	var button5;
+	var buttonS;
 
-	    game.debug.text('world gravity', sprite1.x - 32, 64);
-	    game.debug.text('local gravity', sprite2.x - 32, 64);
-	    game.debug.text('local / 2', sprite3.x - 32, 64);
-	    game.debug.text('no gravity', sprite4.x - 32, 64);
+	var husky1;
+	var husky2;
+	var husky3;
+	var husky4;
+	var husky5;
 
-	}
-
-	///////////////////////////////////////////////////
-	///////////////////////////////////////////////////
-
-	var sprite;
+	var bound1;
+	var bound2;
+	var bound3;
+	var bound4;
+	var bound5;
 
 	var player;
-	var facing = 'left';
-	var jumpTimer = 0;
 	var cursors;
-	var jumpButton;
-	var yAxis = p2.vec2.fromValues(0, 1);
+	var input = -1;
+	var rng = Math.floor(Math.random() * 5) + 1;
 
-	function create() {
-
-	bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-
-	//  Enable p2 physics
-	game.physics.startSystem(Phaser.Physics.P2JS);
-
-	game.physics.p2.gravity.y = 350;
-	game.physics.p2.world.defaultContactMaterial.friction = 0.3;
-	game.physics.p2.world.setGlobalStiffness(1e5);
-
-	//  Add a sprite
-	player = game.add.sprite(200, 200, 'dude');
-	player.animations.add('left', [0, 1, 2, 3], 10, true);
-	player.animations.add('turn', [4], 20, true);
-	player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-	//  Enable if for physics. This creates a default rectangular body.
-	game.physics.p2.enable(player);
-
-	player.body.fixedRotation = true;
-	player.body.damping = 0.5;
-
-	var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', player.body);
-	var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
-	var boxMaterial = game.physics.p2.createMaterial('worldMaterial');
-
-	//  4 trues = the 4 faces of the world in left, right, top, bottom order
-	game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
-
-	//  A stack of boxes - you'll stick to these
-	for (var i = 1; i < 4; i++)
+	function create()
 	{
-	    var box = game.add.sprite(300, 645 - (95 * i), 'atari');
-	    game.physics.p2.enable(box);
-	    box.body.mass = 6;
-	    // box.body.static = true;
-	    box.body.setMaterial(boxMaterial);
+	    game.add.tileSprite(0, 0, 1800, 600, 'background');
+
+	    game.world.setBounds(0, 0, 1800, 500);
+
+	    game.physics.startSystem(Phaser.Physics.P2JS);
+
+	    husky1 = game.add.sprite(150, 50, 'husky', 10);
+	    husky2 = game.add.sprite(150, 150, 'husky', 10);
+	    husky3 = game.add.sprite(150, 250, 'husky', 10);
+	    husky4 = game.add.sprite(150, 350, 'husky', 10);
+	    husky5 = game.add.sprite(150, 450, 'husky', 10);
+
+	    husky1.animations.add('wag');
+    	husky1.animations.play('wag', 8, true);
+    	husky2.animations.add('wag');
+    	husky2.animations.play('wag', 8, true);
+    	husky3.animations.add('wag');
+    	husky3.animations.play('wag', 8, true);
+    	husky4.animations.add('wag');
+    	husky4.animations.play('wag', 8, true);
+    	husky5.animations.add('wag');
+    	husky5.animations.play('wag', 8, true);
+
+	    game.physics.p2.enable(husky1);
+	    game.physics.p2.enable(husky2);
+	    game.physics.p2.enable(husky3);
+	    game.physics.p2.enable(husky4);
+	    game.physics.p2.enable(husky5);
+
+	    bound1 = new Phaser.Rectangle(100, 0, 1700, 100);
+	    bound2 = new Phaser.Rectangle(100, 100, 1700, 100);
+	    bound3 = new Phaser.Rectangle(100, 200, 1700, 100);
+	    bound4 = new Phaser.Rectangle(100, 300, 1700, 100);
+	    bound5 = new Phaser.Rectangle(100, 400, 1700, 100);
+
+	    cursors = game.input.keyboard.createCursorKeys();
+	    
+	    // new Button(game [, x] [, y] [, key] [, callback] [, callbackContext] [, overFrame] [, outFrame] [, downFrame] [, upFrame])
+	    button1 = game.add.button(0, 25, 'arrow', actionOnClick1, this, 0, 1, 0);
+		button2 = game.add.button(0, 125, 'arrow', actionOnClick2, this, 0, 1, 0);
+		button3 = game.add.button(0, 225, 'arrow', actionOnClick3, this, 0, 1, 0);
+		button4 = game.add.button(0, 325, 'arrow', actionOnClick4, this, 0, 1, 0);
+		button5 = game.add.button(0, 425, 'arrow', actionOnClick5, this, 0, 1, 0);
+		buttonS = game.add.button(150, 510, 'button', click_play, this, 0, 0, 1, 0);
+
+	    button1.onInputOver.add(over, this);
+	    button1.onInputOut.add(out, this);
+	    button1.onInputUp.add(up, this);
+
+		button2.onInputOver.add(over, this);
+	    button2.onInputOut.add(out, this);
+	    button2.onInputUp.add(up, this);
+
+		button3.onInputOver.add(over, this);
+	    button3.onInputOut.add(out, this);
+	    button3.onInputUp.add(up, this);
+
+		button4.onInputOver.add(over, this);
+	    button4.onInputOut.add(out, this);
+	    button4.onInputUp.add(up, this);
+
+		button5.onInputOver.add(over, this);
+	    button5.onInputOut.add(out, this);
+	    button5.onInputUp.add(up, this);
+
+
+	    husky1.inputEnabled = true;
+	    husky1.input.boundsRect = bound1;
+	    husky2.input.boundsRect = bound2;
+	    husky3.input.boundsRect = bound3;
+	    husky4.input.boundsRect = bound4;
+	    husky5.input.boundsRect = bound5;
+
 	}
 
-	//  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
-	//  those 2 materials collide it uses the following settings.
-
-	var groundPlayerCM = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial, { friction: 0.0 });
-	var groundBoxesCM = game.physics.p2.createContactMaterial(worldMaterial, boxMaterial, { friction: 0.6 });
-
-	//  Here are some more options you can set:
-
-	// contactMaterial.friction = 0.0;     // Friction to use in the contact of these two materials.
-	// contactMaterial.restitution = 0.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-	// contactMaterial.stiffness = 1e3;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-	// contactMaterial.relaxation = 0;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-	// contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-	// contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-	// contactMaterial.surfaceVelocity = 0.0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
-
-	text = game.add.text(20, 20, 'move with arrow, space to jump', { fill: '#ffffff' });
-
-	cursors = game.input.keyboard.createCursorKeys();
-	jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-	}
-
-	function update() {
-
-	if (cursors.left.isDown)
+	function update()
 	{
-	    player.body.moveLeft(200);
+		if (player != null)
+		{
+		    player.body.setZeroVelocity();
 
-	    if (facing != 'left')
-	    {
-	        player.animations.play('left');
-	        facing = 'left';
-	    }
+		    if (cursors.up.isDown)
+		    {
+		        player.body.moveUp(300)
+		    }
+		    else if (cursors.down.isDown)
+		    {
+		        player.body.moveDown(300);
+		    }
+
+		    if (cursors.left.isDown)
+		    {
+		        player.body.velocity.x = -300;
+		    }
+		    else if (cursors.right.isDown)
+		    {
+		        player.body.moveRight(300);
+		    }
+		}
 	}
-	else if (cursors.right.isDown)
+
+	function render()
 	{
-	    player.body.moveRight(200);
-
-	    if (facing != 'right')
-	    {
-	        player.animations.play('right');
-	        facing = 'right';
-	    }
+	    //game.debug.cameraInfo(game.camera, 32, 32);
 	}
-	else
+
+	function up()
 	{
-	    player.body.velocity.x = 0;
-
-	    if (facing != 'idle')
-	    {
-	        player.animations.stop();
-
-	        if (facing == 'left')
-	        {
-	            player.frame = 0;
-	        }
-	        else
-	        {
-	            player.frame = 5;
-	        }
-
-	        facing = 'idle';
-	    }
+	    console.log('button up', arguments);
 	}
 
-	if (jumpButton.isDown && game.time.now > jumpTimer && checkIfCanJump())
+	function over()
 	{
-	    player.body.moveUp(300);
-	    jumpTimer = game.time.now + 750;
+	    console.log('button over');
 	}
 
-	}
-
-	function checkIfCanJump() {
-
-	var result = false;
-
-	for (var i=0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++)
+	function out()
 	{
-	    var c = game.physics.p2.world.narrowphase.contactEquations[i];
-
-	    if (c.bodyA === player.body.data || c.bodyB === player.body.data)
-	    {
-	        var d = p2.vec2.dot(c.normalA, yAxis);
-
-	        if (c.bodyA === player.body.data)
-	        {
-	            d *= -1;
-	        }
-
-	        if (d > 0.5)
-	        {
-	            result = true;
-	        }
-	    }
+	    console.log('button out');
 	}
 
-	return result;
+	function actionOnClick1()
+	{
+		console.log('click 1');
 
+		button1.setFrames(1, 0, 0, 0);
+
+		switch (input)
+		{
+			case 2:
+				button2.setFrames(0, 1, 0);
+				button2.inputEnabled = true;
+				break;
+			case 3:
+				button3.setFrames(0, 1, 0);
+				button3.inputEnabled = true;
+				break;
+			case 4:
+				button4.setFrames(0, 1, 0);
+				button4.inputEnabled = true;
+				break;
+			case 5:
+				button5.setFrames(0, 1, 0);
+				button5.inputEnabled = true;
+				break;
+			default:
+				break;
+		}
+	    
+		button1.inputEnabled = false;
+		input = 1;
 	}
 
+	function actionOnClick2()
+	{
+		console.log('click 2');
+
+		button2.setFrames(1, 0, 0, 0);
+
+		switch (input)
+		{
+			case 1:
+				button1.setFrames(0, 1, 0);
+				button1.inputEnabled = true;
+				break;
+			case 3:
+				button3.setFrames(0, 1, 0);
+				button3.inputEnabled = true;
+				break;
+			case 4:
+				button4.setFrames(0, 1, 0);
+				button4.inputEnabled = true;
+				break;
+			case 5:
+				button5.setFrames(0, 1, 0);
+				button5.inputEnabled = true;
+				break;
+			default:
+				break;
+		}
+	    
+		button2.inputEnabled = false;
+		input = 2;
+	}
+
+	function actionOnClick3()
+	{
+		console.log('click 3');
+
+		button3.setFrames(1, 0, 0, 0);
+
+		switch (input)
+		{
+			case 1:
+				button1.setFrames(0, 1, 0);
+				button1.inputEnabled = true;
+				break;
+			case 2:
+				button2.setFrames(0, 1, 0);
+				button2.inputEnabled = true;
+				break;
+			case 4:
+				button4.setFrames(0, 1, 0);
+				button4.inputEnabled = true;
+				break;
+			case 5:
+				button5.setFrames(0, 1, 0);
+				button5.inputEnabled = true;
+				break;
+			default:
+				break;
+		}
+	    
+		button3.inputEnabled = false;
+		input = 3;
+	}
+
+	function actionOnClick4()
+	{
+		console.log('click 4');
+
+		button4.setFrames(1, 0, 0, 0);
+
+		switch (input)
+		{
+			case 1:
+				button1.setFrames(0, 1, 0);
+				button1.inputEnabled = true;
+				break;
+			case 2:
+				button2.setFrames(0, 1, 0);
+				button2.inputEnabled = true;
+				break;
+			case 3:
+				button3.setFrames(0, 1, 0);
+				button3.inputEnabled = true;
+				break;
+			case 5:
+				button5.setFrames(0, 1, 0);
+				button5.inputEnabled = true;
+				break;
+			default:
+				break;
+		}
+	    
+		button4.inputEnabled = false;
+		input = 4;
+	}
+
+	function actionOnClick5()
+	{
+		console.log('click 5');
+
+		button5.setFrames(1, 0, 0, 0);
+
+		switch (input)
+		{
+			case 1:
+				button1.setFrames(0, 1, 0);
+				button1.inputEnabled = true;
+				break;
+			case 2:
+				button2.setFrames(0, 1, 0);
+				button2.inputEnabled = true;
+				break;
+			case 3:
+				button3.setFrames(0, 1, 0);
+				button3.inputEnabled = true;
+				break;
+			case 4:
+				button4.setFrames(0, 1, 0);
+				button4.inputEnabled = true;
+				break;
+			default:
+				break;
+		}
+	    
+		button5.inputEnabled = false;
+		input = 5;
+	}
+
+	function click_play()
+	{
+		console.log("Play");
+
+		if (input == -1)
+		{
+			//Do nothing.
+		}
+		else
+		{
+			switch (input)
+			{
+				case 1:
+					player = husky1;
+					break;
+				case 2:
+					player = husky2;
+					break;
+				case 3:
+					player = husky3;
+					break;
+				case 4:
+					player = husky4;
+					break;
+				case 5:
+					player = husky5;
+					break;
+				default:
+					break;
+			}
+
+			buttonS.inputEnabled = false;	
+		    //  Notice that the sprite doesn't have any momentum at all,
+		    //  it's all just set by the camera follow type.
+		    //  0.1 is the amount of linear interpolation to use.
+		    //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
+		    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+		    player.body.fixedRotation = true;
+
+			husky1.body.moveRight(300);
+			husky2.body.moveRight(300);
+			husky3.body.moveRight(300);
+			husky4.body.moveRight(300);
+			husky5.body.moveRight(300);
+
+		}
+	}
 };
