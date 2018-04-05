1dbf260439a37b27cbd5cb3dd4ffb927762adaa2
jlee184 <jlee184@gmu.edu>
2018-04-05 10:58:53 -0400

    Added some sprites

--- Iteration Sequence 1/js/main.js
+++ Iteration Sequence 1/js/main.js
@@ -2,65 +2,67 @@
 
 window.onload = function()
 {
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-
-    // mods by Patrick OReilly 
-    // twitter: @pato_reilly
-
 	var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 	function preload()
 	{		
-	    game.load.image('background','assets/temp_bg');
+	    game.load.image('background','assets/temp_bg.jpg');
+	    game.load.spritesheet('tile','assets/sheet.png', 70, 70);
+	    game.load.spritesheet('man','assets/adventurer_tilesheet.png', 80, 110);
+	    //game.load.atlasJSONHash('bot', 'assets/sprites/running_bot.png', 'assets/sprites/running_bot.json');
 	}
 
 	var button1;
+	var button2;
+	var button3;
+	var button4;
+	var button5;
+	var button6;
 
 	var player;
 	var cursors;
-	var input = -1;
-	var rng = Math.floor(Math.random() * 5) + 1;
 
 	function create()
 	{
-		game.add.tileSprite(0, 0, 1800, 600, 'background');
-		game.world.setBounds(0, 0, 1800, 500);
-	    cursors = game.input.keyboard.createCursorKeys();    	    
+		game.add.tileSprite(0, 0, 1300, 650, 'background');
+		//game.world.setBounds(0, 0, 1800, 500);
+	    cursors = game.input.keyboard.createCursorKeys();
+
+	    button1 = game.add.sprite(250, 500, 'tile', 1);
+	    button2 = game.add.sprite(400, 500, 'tile', 1);
+	    button3 = game.add.sprite(550, 500, 'tile', 1);
+	    button4 = game.add.sprite(700, 500, 'tile', 1);
+	    button5 = game.add.sprite(850, 500, 'tile', 1);
+	    button6 = game.add.sprite(1000, 500, 'tile', 1);
+	    
+	    player = game.add.sprite(100, 450, 'man', 1);
+	    player.anchor.setTo(0.5, 0.5);
+	    player.scale.setTo(1, 1);
+
+	    player.animations.add('run', [1,9,10]);
+	    //player.animations.play('run', 3, true);
 	}
 
+
 	function update()
-	{
-		if (player != null)
-		{
-		    player.body.setZeroVelocity();
+	{			
+	    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+	    {
+	        player.x -= 4;
+	    }
+	    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+	    {
+	        player.x += 4;
+	    }
 
-		    if (cursors.up.isDown)
-		    {
-		        player.body.moveUp(300)
-		    }
-		    else if (cursors.down.isDown)
-		    {
-		        player.body.moveDown(300);
-		    }
-
-		    if (cursors.left.isDown)
-		    {
-		        player.body.velocity.x = -300;
-		    }
-		    else if (cursors.right.isDown)
-		    {
-		        player.body.moveRight(300);
-		    }
-		}
-
-		game.physics.arcade.collide(huskies, boundaries, slow_down, null, this);
+	    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
+	    {
+	        player.y -= 4;
+	    }
+	    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+	    {
+	        player.y += 4;
+	    }
 	}
 
 	function render()
