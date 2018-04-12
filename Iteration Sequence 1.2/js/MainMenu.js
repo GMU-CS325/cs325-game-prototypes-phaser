28899e90fd5acf6f4cc8d45d4ca23515fffbe2ed
jlee184 <jlee184@gmu.edu>
2018-04-12 01:05:15 -0400

    Trying to fix Game.js file error

--- Iteration Sequence 1.2/js/MainMenu.js
+++ Iteration Sequence 1.2/js/MainMenu.js
@@ -1,30 +1,27 @@
 "use strict";
 
-GameStates.makeMainMenu = function( game, shared )
-{
+GameStates.makeMainMenu = function( game, shared ) {
 
-	var music = null;
-	var playButton = null;
+    var music = null;
+    var playButton = null;
     
-    function startGame(pointer)
-    {
+    function startGame(pointer) {
 
-        //	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+        //  Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
         music.stop();
 
-        //	And start the actual game
+        //  And start the actual game
         game.state.start('Game');
 
     }
     
     return {
     
-        create: function ()
-        {
+        create: function () {
     
-            //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
-            //	Here all we're doing is playing some music and adding a picture and button
-            //	Naturally I expect you to do something significantly better :)
+            //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
+            //  Here all we're doing is playing some music and adding a picture and button
+            //  Naturally I expect you to do something significantly better :)
     
             music = game.add.audio('titleMusic');
             music.play();
@@ -35,10 +32,9 @@ GameStates.makeMainMenu = function( game, shared )
     
         },
     
-        update: function ()
-        {
+        update: function () {
     
-            //	Do some nice funky main menu effect here
+            //  Do some nice funky main menu effect here
     
         }
         
