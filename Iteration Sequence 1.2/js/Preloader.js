0a0cb78a4aa15f3cd6bd1f1ede62f430393b360b
jlee184 <jlee184@gmu.edu>
2018-04-12 00:43:49 -0400

    Test template

--- Iteration Sequence 1.2/js/Preloader.js
+++ Iteration Sequence 1.2/js/Preloader.js
@@ -51,12 +51,18 @@ GameStates.makePreloader = function( game )
             //	If you don't have any music in your game then put the game.state.start line into the create function and delete
             //	the update function completely.
             
+            
             if (game.cache.isSoundDecoded('titleMusic') && ready == false)
             {
                 ready = true;
                 game.state.start('MainMenu');
             }
-    
+            /*    
+            if (ready == false)
+            {
+                ready = true;
+                game.state.start('MainMenu');
+            }*/
         }
     
     };
