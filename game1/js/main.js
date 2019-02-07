858401bdad454d5cbc2b6094aff4dc9379058aaa
kkim45 <47230708+kkim45@users.noreply.github.com>
2019-02-07 14:02:42 -0500

    Update main.js

--- game1/js/main.js
+++ game1/js/main.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 
 function preload() {
