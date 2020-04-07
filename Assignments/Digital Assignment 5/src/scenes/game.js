572152b81ef69d9c98fbc84c0528133104ed7753
zainulabideenfazal <zainkfazal@gmail.com>
2020-04-07 14:08:11 -0400

    Added digital assignment 5

---
+++ Assignments/Digital Assignment 5/src/scenes/game.js
@@ -0,0 +1,34 @@
+var scene = new Phaser.Scene("game");
+
+import TitleScene from './titleScene.js';
+import GameScene from './gameScene.js';
+import PreloadScene from './preloadScene.js';
+
+// Our game scene
+var gameScene = new GameScene();
+var titleScene = new TitleScene();
+var preloadScene = new PreloadScene();
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 720,
+    physics: {
+        default: "matter",
+        matter: {
+            debug: true
+        },
+        /*gravity: {
+            y: 0.05
+        }*/
+    }
+};
+var game = new Phaser.Game(config);
+
+// load scenes
+game.scene.add('preloadScene', preloadScene);
+game.scene.add('titleScene', titleScene);
+game.scene.add("game", gameScene);
+
+// start title
+game.scene.start('preloadScene');
