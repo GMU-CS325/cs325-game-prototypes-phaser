0ba7e94b54cb3ab265b2651fe537796db736339e
zainulabideenfazal <zainkfazal@gmail.com>
2020-04-09 09:00:24 -0400

    Wryyyyy

--- Assignments/Digital Assignment 5/src/scenes/gameScene.js
+++ Assignments/Digital Assignment 5/src/scenes/gameScene.js
@@ -1,8 +1,8 @@
 var shapes;
+var stickshapes;
 var background;
-var ground;
-var obstacles;
 var frameCounter;
+var obstacles;
 
 class GameScene extends Phaser.Scene {
 
@@ -15,39 +15,71 @@ class GameScene extends Phaser.Scene {
 	};
 
 	preload() {
-		 
 	}
 
 	create() {
+		//var timer = this.time.delayedCall(30000, callback, args, scope); for the countdown event
 		this.anims.create(
 			{
 				key: 'level_background',
 				frames: [
-					{ key: 'level_bg_001' },
-					{ key: 'level_bg_002' },
-					{ key: 'level_bg_003' },
-					{ key: 'level_bg_004' },
-					{ key: 'level_bg_005' },
-					{ key: 'level_bg_006' },
-					{ key: 'level_bg_007' },
-					{ key: 'level_bg_008' },
+					{ key: 'bg1' },
+					{ key: 'bg2' },
+					{ key: 'bg3' },
+					{ key: 'bg4' },
+					{ key: 'bg5' },
+					{ key: 'bg6' },
+					{ key: 'bg7' },
+					{ key: 'bg8' },
 				],
 				frameRate: 10,
 				repeat: -1
 			});
-		this.matter.world.setBounds(0, 0, 1200, 720);
-		background = this.matter.add.sprite(600, 360, 'level_bg_001', { isStatic: true, ignoreGravity: true }).play('level_background');
-		//ground = this.matter.add.sprite(600, 800, 'ground', null, { isStatic: true, ignoreGravity: true }).setDepth(2).setOrigin(0, 0);
-		console.log(background.body);
-		//background.body.setCollideWorldBounds(true);
-		shapes = this.cache.json.get('shapes');
+		this.matter.world.setBounds(0, -100, 1200, 770, 64, true, true, false, true);
+		background = this.add.sprite(600, 360, 'bg1').play('level_background');
 
-		//var obstacle = this.matter.add.sprite(Phaser.Math.Between(0, 1200), 250, 'sheet', 'spikyball');
+		shapes = this.cache.json.get('ashapes');
+		stickshapes = this.cache.json.get('sshapes');
+
+		obstacles = this.add.group({
+			key: 'obstacles',
+			maxSize: 15, //Only 14 spawned?
+			//setXY: { x: Phaser.Math.Between(0, 1200), y: -50 }
+			//classType: Phaser.Physics.Matter.Image(this.world, 0, 0, null, null, {}),
+		});
+		console.log(shapes);
+		console.log(stickshapes);
+		//this.matter.add.sprite(300, -20, 'asheet', 'ball', { shape: shapes.ball })
+		this.matter.add.sprite(300, -20, 'ssheet', 'P1', { shape: shapes.P1 })
 	}
 
 	update() {
 		frameCounter = frameCounter + 1;
-		
+		if ( (frameCounter % 125 == 0) && (obstacles.isFull() == false)) {
+			var x = Math.floor(Math.random() * 5);
+			switch (x) {
+				case 0:
+					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -20, 'asheet', 'ball', { shape: shapes.ball }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 1:
+					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'box', { shape: shapes.box }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 2:
+					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'manyspike', { shape: shapes.manyspike }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 3:
+					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'spike', { shape: shapes.spike }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 4:
+					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'spikyball', { shape: shapes.spikyball }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+			}
+		}
 	}
 
 
