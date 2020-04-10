06bea5d16ea2fce5b298b6f57ebb312425e7324e
zainulabideenfazal <zainkfazal@gmail.com>
2020-04-09 22:27:15 -0400

    Mostly working

--- Assignments/Digital Assignment 5/src/scenes/gameScene.js
+++ Assignments/Digital Assignment 5/src/scenes/gameScene.js
@@ -3,6 +3,15 @@ var stickshapes;
 var background;
 var frameCounter;
 var obstacles;
+var cursors;
+var jackA;
+var jackB;
+var WASD;
+var cat1;
+
+function logA() {
+	console.log('HitA');
+}
 
 class GameScene extends Phaser.Scene {
 
@@ -14,11 +23,15 @@ class GameScene extends Phaser.Scene {
 		frameCounter = 0;
 	};
 
+	
+
 	preload() {
 	}
 
 	create() {
 		//var timer = this.time.delayedCall(30000, callback, args, scope); for the countdown event
+		cursors = this.input.keyboard.createCursorKeys();
+		WASD = this.input.keyboard.addKeys('W,A,S,D,TAB');
 		this.anims.create(
 			{
 				key: 'level_background',
@@ -35,6 +48,43 @@ class GameScene extends Phaser.Scene {
 				frameRate: 10,
 				repeat: -1
 			});
+
+		this.anims.create({
+			key: 'jump',
+			frameRate: 10,
+			frames: this.anims.generateFrameNames('jack', {
+				prefix: 'bjack_',
+				suffix: '.png',
+				start: 21,
+				end: 29,
+				zeroPad: 2
+			})
+		});
+
+		this.anims.create({
+			key: 'run',
+			frameRate: 10,
+			frames: this.anims.generateFrameNames('jack', {
+				prefix: 'bjack_',
+				suffix: '.png',
+				start: 1,
+				end: 5,
+				zeroPad: 2
+			})
+		});
+
+		this.anims.create({
+			key: 'idle',
+			frameRate: 10,
+			frames: this.anims.generateFrameNames('jack', {
+				prefix: 'bjack_',
+				suffix: '.png',
+				start: 5,
+				end: 8,
+				zeroPad: 2
+			})
+		});
+		
 		this.matter.world.setBounds(0, -100, 1200, 770, 64, true, true, false, true);
 		background = this.add.sprite(600, 360, 'bg1').play('level_background');
 
@@ -43,24 +93,75 @@ class GameScene extends Phaser.Scene {
 
 		obstacles = this.add.group({
 			key: 'obstacles',
-			maxSize: 15, //Only 14 spawned?
+			maxSize: 4, //Only 14 spawned?
 			//setXY: { x: Phaser.Math.Between(0, 1200), y: -50 }
 			//classType: Phaser.Physics.Matter.Image(this.world, 0, 0, null, null, {}),
+			createCallback: function (obstacle) {
+				obstacle.setName('Obstacle ' + this.getLength());
+				console.log(obstacle.name);
+			}
 		});
-		console.log(shapes);
-		console.log(stickshapes);
 		//this.matter.add.sprite(300, -20, 'asheet', 'ball', { shape: shapes.ball })
-		this.matter.add.sprite(300, -20, 'ssheet', 'P1', { shape: shapes.P1 })
+		jackB = this.matter.add.sprite(100, 620, 'jack', 'bjack_06.png').setScale(3);
+		jackB.body.gameObject.name = "P1";
+		console.log(jackB.body.gameObject.name);
+		jackA = this.matter.add.sprite(1100, 620, 'jack', 'bjack_06.png').setScale(3).setFlipX(true);
+		jackA.body.gameObject.name = "P2";
+
+		jackA.on('animationcomplete', function (animation, frame) {
+			if (animation.key === 'jump') {
+				console.log("He jumpth");
+			}
+			else if (animation.key === 'run') {
+				jackA.play('idle');
+			}
+		}, this);
+
+		jackB.on('animationcomplete', function (animation, frame) {
+			if (animation.key === 'jump') {
+				console.log("He jumpth");
+			}
+			else if (animation.key === 'run') {
+				jackB.play('idle');
+			}
+		}, this);
+
+		
+		this.matter.world.on('collisionstart', function (event) {
+		})
+
+		jackA.setOnCollide(function (MatterCollisionData) {
+			if ((MatterCollisionData.bodyA.gameObject) && (MatterCollisionData.bodyB.gameObject) ) {
+				if ((MatterCollisionData.bodyA.gameObject.name === "P2") && (MatterCollisionData.bodyB.gameObject.texture.key === "asheet"))
+					console.log("P2 hit");
+				else if ((MatterCollisionData.bodyB.gameObject.name === "P2") && (MatterCollisionData.bodyA.gameObject.texture.key === "asheet"))
+					console.log("P2 hit");
+			}
+		})
+
+		jackB.setOnCollide(function (MatterCollisionData) {
+			if ((MatterCollisionData.bodyA.gameObject) && (MatterCollisionData.bodyB.gameObject)) {
+				if ((MatterCollisionData.bodyA.gameObject.name === "P1") && (MatterCollisionData.bodyB.gameObject.texture.key === "asheet"))
+					console.log("P1 hit");
+				else if ((MatterCollisionData.bodyB.gameObject.name === "P1") && (MatterCollisionData.bodyA.gameObject.texture.key === "asheet"))
+					console.log("P1 hit");
+			}
+		})
+
 	}
 
 	update() {
 		frameCounter = frameCounter + 1;
+		/*obstacles.children.each(function (obstacle) {
+			console.log(obstacle);
+		}, this);*/
 		if ( (frameCounter % 125 == 0) && (obstacles.isFull() == false)) {
 			var x = Math.floor(Math.random() * 5);
 			switch (x) {
 				case 0:
 					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -20, 'asheet', 'ball', { shape: shapes.ball }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
 					obstacles.add(obstacle);
+					//console.log(obstacle.body);
 					break;
 				case 1:
 					var obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'box', { shape: shapes.box }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
@@ -79,7 +180,75 @@ class GameScene extends Phaser.Scene {
 					obstacles.add(obstacle);
 					break;
 			}
+			
 		}
+
+		if (cursors.left.isDown) {
+			jackA.setVelocityX(-7);
+			jackA.setFlipX(true);
+			if (jackA.body.position.y > 610) {
+				
+				if (jackA.anims.getCurrentKey() != 'run') {
+					jackA.play('run');
+				}
+			}
+		}
+		else if (cursors.right.isDown) {
+			jackA.setVelocityX(7);
+			jackA.setFlipX(false);
+			if (jackA.body.position.y > 610) {
+				
+				if (jackA.anims.getCurrentKey() != 'run') {
+					jackA.play('run');
+				}
+			}
+		}
+		else {
+			jackA.setVelocityX(0);
+			//jackA.play('idle');
+		}
+
+		if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
+			if (jackA.body.position.y > 610) {
+				jackA.play('jump');
+				jackA.setVelocityY(-13);	
+			}
+			
+		}
+
+		if (WASD.A.isDown) {
+			jackB.setVelocityX(-7);
+			jackB.setFlipX(true);
+			if (jackB.body.position.y > 610) {
+				
+				if (jackB.anims.getCurrentKey() != 'run') {
+					jackB.play('run');
+				}
+			}
+		}
+		else if (WASD.D.isDown) {
+			jackB.setVelocityX(7);
+			jackB.setFlipX(false);
+			if (jackB.body.position.y > 610) {
+				
+				if (jackB.anims.getCurrentKey() != 'run') {
+					jackB.play('run');
+				}
+			}
+		}
+		else {
+			jackB.setVelocityX(0);
+			jackB.play('idle');
+		}
+
+		if (Phaser.Input.Keyboard.JustDown(WASD.W)) {
+			if (jackB.body.position.y > 610) {
+				jackB.play('jump');
+				jackB.setVelocityY(-13);
+			}
+
+		}
+
 	}
 
 
