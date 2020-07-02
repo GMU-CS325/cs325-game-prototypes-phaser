118702fed5144305c60efd617f76f568ab95125a
zainulabideenfazal <zainkfazal@gmail.com>
2020-07-02 17:12:58 -0400

    Deciding to work with Yoshi instead of Jack

---
+++ Assignments/Digital Assignment 9/src/scenes/gameScene.js
@@ -0,0 +1,112 @@
+var shapes;
+var WASD;
+var testshapes;
+var background;
+var frameCounter;
+var obstacles;
+var cursors;
+var player1;
+var player2;
+
+var GameScene = new Phaser.Class({
+
+	Extends: Phaser.Scene,
+	initialize:
+
+		function GameScene() {
+			Phaser.Scene.call(this, { key: 'gameScene' });
+		},
+
+	init: function() {
+		frameCounter = 0;
+	},
+
+	create: function () {
+
+		cursors = this.input.keyboard.createCursorKeys();
+		WASD = this.input.keyboard.addKeys('W,A,S,D,TAB');
+		this.anims.create(
+			{
+				key: 'level_background',
+				frames: [
+					{ key: 'bg1' },
+					{ key: 'bg2' },
+					{ key: 'bg3' },
+					{ key: 'bg4' },
+					{ key: 'bg5' },
+					{ key: 'bg6' },
+					{ key: 'bg7' },
+					{ key: 'bg8' },
+				],
+				frameRate: 10,
+				repeat: -1
+			});
+
+		this.matter.world.setBounds(0, -100, 1200, 770, 64, true, true, false, true);
+		background = this.add.sprite(600, 360, 'bg1').play('level_background');
+
+		//this.matter.add.sprite(300, -20, 'asheet', 'ball', { shape: shapes.ball })
+		player1 = new Player(this, 100, 520, 'testsheet', 'bjack_06.png');
+		player2 = new Player(this, 1100, 520, 'testsheet', 'bjack_06.png').setFlipX(true);
+
+		shapes = this.cache.json.get('ashapes');
+		testshapes = this.cache.json.get('testshapes');
+
+		obstacles = this.add.group({
+			key: 'obstacles',
+			maxSize: 2, //Only 3 spawned?
+			//classType: Phaser.Physics.Matter.Image(this.world, 0, 0, null, null, {}),
+		});
+
+		/*this.matter.world.on('collisionactive', function (bodyA, bodyB) {
+			if (bodyA.label != "Rectangle Body" || bodyB.label != "Rectangle Body")
+				console.log("Ground hit!");
+			else
+				console.log("Yamete!");
+        })*/
+	},
+
+	update: function (time, delta) {
+		player2.update(time, delta);
+		frameCounter = frameCounter + 1;
+		if ((frameCounter % 125 == 0) && (obstacles.isFull() == false)) {
+			var x = Math.floor(Math.random() * 5);
+			var obstacle;
+			switch (x) {
+				case 0:
+					obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -20, 'asheet', 'ball', { shape: shapes.ball }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 1:
+					obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'box', { shape: shapes.box }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 2:
+					obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'manyspike', { shape: shapes.manyspike }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 3:
+					obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'spike', { shape: shapes.spike }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+				case 4:
+					obstacle = this.matter.add.sprite(Phaser.Math.Between(300, 900), -50, 'asheet', 'spikyball', { shape: shapes.spikyball }).setVelocity(Phaser.Math.Between(-5, 5), Phaser.Math.Between(-30, 30));
+					obstacles.add(obstacle);
+					break;
+			}
+			obstacle.setBounce(1.25);
+			
+			var timer = this.time.addEvent({
+				delay: 6000,
+				callback: function () {
+					obstacles.killAndHide(obstacle);
+					obstacle.destroy();
+				},
+			})
+
+		}
+	}
+
+});
+
+export default GameScene;
---
+++ Assignments/Digital Assignment 9/src/scenes/player.js
@@ -0,0 +1,113 @@
+var cursors;
+var player;
+class Player extends Phaser.Physics.Matter.Sprite {
+    constructor(scene, x, y, texture, frame ) {
+        super(scene.matter.world, x, y, texture, frame);
+        scene.add.existing(this);
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+        this.sprite = this;
+        player = this;
+        console.log(this.sprite);
+        this.scene = scene;
+        this.setPosition(x, y);
+        this.setTexture(texture);
+        this.setFrame(frame);
+
+        cursors = scene.input.keyboard.createCursorKeys();
+        this.setData("health", 100);
+
+        scene.anims.create({
+            key: 'jack-jump',
+            frameRate: 10,
+            frames: scene.anims.generateFrameNames('testsheet', {
+                prefix: 'bjack_',
+                suffix: '.png',
+                start: 21,
+                end: 29,
+                zeroPad: 2
+            })
+        });
+
+        scene.anims.create({
+            key: 'jack-run',
+            frameRate: 5,
+            //repeat: -1,
+            frames: scene.anims.generateFrameNames('testsheet', {
+                prefix: 'bjack_',
+                suffix: '.png',
+                start: 1,
+                end: 5,
+                zeroPad: 2
+            })
+        });
+
+        scene.anims.create({
+            key: 'jack-idle',
+            frameRate: 60,
+            frames: scene.anims.generateFrameNames('testsheet', {
+                prefix: 'bjack_',
+                suffix: '.png',
+                start: 5,
+                end: 5,
+                zeroPad: 2
+            })
+        });
+
+        this.play("jack-idle");
+        this.on('animationcomplete', function (animation, frame) {
+            if (animation.key === 'jack-jump') {
+                console.log("He jumpth");
+            }
+            else if (animation.key === 'jack-run') {
+                console.log("He runth");
+                player.play("jack-idle");
+            }
+        }, scene);
+    }
+
+    jump() {
+        if (this.body.position.y > 610) {
+            //this.setTexture("jump");
+            this.play('jack-jump');
+            this.setVelocityY(-13);
+        }
+        
+    }
+
+    moveLeft() {
+        this.x += -7;
+        this.setFlipX(true);
+        if (this.body.position.y > 610) {
+            this.play('jack-run', true);
+        }
+    }
+
+    moveRight() {
+        this.x += 7;
+        this.setFlipX(false);
+        if (this.body.position.y > 610) {
+            this.play('jack-run', true);
+        }
+    }
+
+    update(time, delta) {
+        //console.log(this.anims.getCurrentKey());
+        if (cursors.left.isDown)
+            this.moveLeft();
+        else if (cursors.right.isDown)
+            this.moveRight();
+        else {
+            this.setVelocityX(0);
+            //this.play("idlea");
+        }
+
+        /*if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            if (this.body.position.y > 610) {
+                this.play('jump');
+                this.setVelocityY(-13);
+            }
+
+        }*/
+    }
+}
